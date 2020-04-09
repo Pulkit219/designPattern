8e9baced0bcb8b93cea8b3c3ade79a76209d645d
Ascension1110 <ascension1110@gmail.com>
2020-04-09 02:44:13 -0400

    1st commit

--- compositePattern/src/com/pattern/composite/CompositeTest.java
+++ compositePattern/src/com/pattern/composite/CompositeTest.java
@@ -4,5 +4,14 @@ public class CompositeTest {
 
     public static void main(String[] args) {
 
+        Component hd=new Leaf(100,"HDD");
+        Component ram=new Leaf(500,"RAM");
+
+        Composite motherB=new Composite("motherb");
+        motherB.addComponent(hd);
+        motherB.addComponent(ram);
+//        motherB.showPrice();
+
+        hd.showPrice();
     }
 }
--- compositePattern/src/com/pattern/composite/ComputerPart.java
+++ compositePattern/src/com/pattern/composite/ComputerPart.java
@@ -1,4 +1,53 @@
 package com.pattern.composite;
 
-public class ComputerPart {
+
+import java.util.*;
+
+interface Component{
+     void showPrice();
+}
+
+
+
+ class Leaf implements Component{
+    int price;
+    String name;
+
+     public Leaf(int price, String name) {
+         this.price = price;
+         this.name = name;
+     }
+
+     @Override
+     public void showPrice() {
+         System.out.println(this.name +" "+this.price);
+     }
+
+     public void  test(){
+         System.out.println("test");
+     }
+ }
+
+
+ class Composite implements Component {
+
+     String name;
+     public Composite(String name) {
+         this.name = name;
+     }
+
+
+    List<Component> compList=new ArrayList<>();
+
+    public void addComponent(Component l){
+        compList.add(l);
+    }
+     @Override
+     public void showPrice() {
+         System.out.println(this.name);
+         for(Component c:compList){
+             c.showPrice();
+         }
+     }
+
 }
